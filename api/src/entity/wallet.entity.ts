8f71b0e9345c5c0a183ea8a9a6681fce6125cabf
themsiqueira <mateussiqueiracarneiro@outlook.com>
2023-08-19 18:25:39 -0300

    feat: add entities skeletoon

---
+++ api/src/entity/wallet.entity.ts
@@ -0,0 +1,15 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm'
+
+@Entity()
+export class WalletEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column()
+  @CreateDateColumn()
+  createdAt: Date
+
+  @Column()
+  @UpdateDateColumn()
+  updatedAt: Date
+}

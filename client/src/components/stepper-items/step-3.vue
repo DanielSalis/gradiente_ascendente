477ae54804f22afe1eae4236e16686b59413ab9e
DanielSalis <danielsalis98@gmail.com>
2023-08-20 03:46:36 -0300

    feat: start quiz

--- client/src/components/stepper-items/step-3.vue
+++ client/src/components/stepper-items/step-3.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+    <div
+      v-for="(item, index) in response"
+      :key="index"
+    >
+      <span>{{ item.title }}</span>
+      <div
+        v-for="(answer, annsindex) in item.alternatives"
+        :key="annsindex"
+        @click="handleAlternativeClick(item, answer)"
+      >
+        <span>{{ answer }}</span>
+      </div>
+    </div>
 
     <v-divider />
   </div>
@@ -8,6 +20,47 @@
 
 <script>
   export default {
-    name: 'Step3Item'
+    name: 'Step3Item',
+    data() {
+      return {
+        rightAnswers: 0,
+        response: [
+          {
+            'title': 'Teste',
+            'alternatives': [
+              "a) teste teste",
+              "b) teste teste 2",
+              "c) teste teste 3",
+            ],
+            'answer': 'a) teste teste'
+          },
+          {
+            'title': 'Teste2',
+            'alternatives': [
+              "a) teste teste",
+              "b) teste teste 2",
+              "c) teste teste 3",
+            ],
+            'answer': 'a) teste teste'
+          }
+        ]
+      }
+    },
+    computed:{
+      answers() {
+        return this.rightAnswers === this.response.length
+      }
+    },
+    methods: {
+      handleAlternativeClick(item, answer){
+        console.log(answer)
+        if (answer === item.answer) {
+          this.rightAnswers++;
+          console.log("acertou");
+        }
+      }
+    }
   }
 </script>
+<style>
+</style>
